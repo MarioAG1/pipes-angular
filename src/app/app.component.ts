ff4797091dff6ad15546e21a7368890e5b902c95
MarioAG <marioalvarezgonzalez21@gmail.com>
2025-12-24 12:46:40 +0100

    150. Lectura de rutas dinamicamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
 })
 export class AppComponent {
   protected readonly title = signal('pipes-app');
