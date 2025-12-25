bb2c1c1e6516a475497098f0f062f84ec0a8a3dd
MarioAG <marioalvarezgonzalez21@gmail.com>
2025-12-25 13:14:34 +0100

    153 Date pipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,13 +1,23 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
   nameLower = signal('mario');
   nameUpper = signal('MARIO');
   fullName = signal('MaRiO aLVArez');
+
+  customDate = signal(new Date());
+  tickingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+    onCleanUp(() => {
+      clearInterval(interval);
+    });
+  });
 }
