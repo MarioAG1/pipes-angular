9e619854c7e3e3afca24f32ca5a2e1bf519e9ac3
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-09 16:56:37 +0100

    170. Proyecto pipes tabla

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
 
 @Component({
   selector: 'app-custom-page',
@@ -9,6 +10,7 @@ import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 export default class CustomPageComponent {
   name = signal('Mario');
   uppercase = signal<boolean>(true);
+  heroes = signal(heroes);
 
   changeCase() {
     if (this.uppercase() === true) {
