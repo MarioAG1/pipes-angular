7ae1c4b8c7b6108e9224fa7ecbefeef90f624f17
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-10 16:52:33 +0100

    174. Pipe Personalizado - HeroFilterPipe

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,5 +1,5 @@
 import { Hero } from './../../interfaces/hero.interface';
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
@@ -9,6 +9,7 @@ import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 import { Creator } from '../../interfaces/hero.interface';
 import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { heroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
@@ -20,6 +21,7 @@ import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
     TitleCasePipe,
     HeroCreatorPipe,
     HeroSortByPipe,
+    heroFilterPipe,
   ],
   templateUrl: './custom-page.component.html',
 })
@@ -29,6 +31,7 @@ export default class CustomPageComponent {
   heroes = signal(heroes);
   DC = Creator.DC;
   sortBy = signal<keyof Hero | null>(null);
+  searchQuery = signal('');
 
   changeCase() {
     if (this.uppercase() === true) {
@@ -41,4 +44,20 @@ export default class CustomPageComponent {
   // changeCase() {
   //   this.uppercase.set(!this.uppercase());
   // }
+
+  // Para comprobar si el valor esta vacio o no y sustituirlo, se puede hacer asi
+  comprobarQuery() {
+    const valor = this.searchQuery().trim();
+    if (valor === '') {
+      return 'XXX';
+    } else return valor;
+  }
+
+  // Asi es la forma computada, que solo se activara si el searchQuery() cambia
+  // comprobarQuery = computed(() => {
+  //   const valor = this.searchQuery().trim();
+  //   if (valor === '') {
+  //     return 'XXX';
+  //   } else return valor;
+  // });
 }
